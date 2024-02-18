59c17daab1b27930044c879fea4203520d55ff25
DjordjePavlicevic <dj.pavlicevic@gmail.com>
2024-02-18 11:43:54 +0100

    Fixed serializer so Subtask is not mandatory

--- app/projects/serializers.py
+++ app/projects/serializers.py
@@ -1,7 +1,8 @@
 """ Serializers for Projects and SubProjects"""
 from rest_framework import serializers
-from core.models import Project, SubProject
+from core.models import Project, SubProject, Task
 from tasks.serializers import TaskDetailSerializer
+
 
 
 class SubProjectSerializer(serializers.ModelSerializer):
@@ -27,11 +28,19 @@ class ProjectSerializer(serializers.ModelSerializer):
 
 
 class ProjectDetailSerializer(ProjectSerializer):
-    sub_projects = SubProjectDetailSerializer(many=True)
-    tasks = TaskDetailSerializer(many=True, read_only=True, source='task_set')
+    sub_projects = serializers.SerializerMethodField()
+    tasks = serializers.SerializerMethodField()
 
     class Meta(ProjectSerializer.Meta):
         fields = ProjectSerializer.Meta.fields + ['description', 'sub_projects', 'tasks']
+
+    def get_sub_projects(self, obj):
+        sub_projects = SubProject.objects.filter(project=obj)
+        return SubProjectSerializer(sub_projects, many=True).data
+
+    def get_tasks(self, obj):
+        tasks = Task.objects.filter(project=obj)
+        return TaskDetailSerializer(tasks, many=True).data
 
 
 
